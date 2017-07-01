9343db0de0df9c2960abe7337fa5e118dcf957be
Tadeusz <tadeusz.m.lewandowski@gmail.com>
2017-07-01 18:44:07 +0200

    Start project

---
+++ content_scripts/form.js
@@ -0,0 +1,35 @@
+
+let getURL = () => window.location.href;
+
+let isYoutube = (url) => {
+  return url.search("youtube") != -1 ? true : false;
+}
+
+let youtubeData = () => {
+  let time = document.querySelector(".ytp-time-current").textContent;
+  let title = document.querySelector(".watch-title").textContent;
+  return {time, title};
+}
+
+let info = (request, sender, sendResponse) => {
+
+  let url = getURL();
+  let movieData = {};
+
+  if(isYoutube(url)) {
+    movieData = youtubeData();
+    movieData.url = url;
+  } else {
+    movieData = {
+      title: "",
+      url,
+      time: ""
+    }
+  }
+
+  sendResponse({movie: movieData});
+
+  // browser.runtime.onMessage.removeListener(info);
+}
+
+browser.runtime.onMessage.addListener(info);
---
+++ popup/index.js
@@ -0,0 +1,169 @@
+
+
+let clearList = (list) => {
+  while(list.childNodes.length) {
+    list.removeChild(list.lastChild);
+  }
+}
+
+let reverseArray = (array) => {
+  return array.reduce((a, b) => [b].concat(a), [])
+}
+
+
+
+
+
+let fillForm = (form, movie) => {
+  form.elements.namedItem("title").value = movie.title.trim();
+  form.elements.namedItem("url").value = movie.url;
+  form.elements.namedItem("time").value = movie.time;
+}
+
+let checkMovie = (movie) => {
+  // if it's YT movie
+  if(movie.url.search("youtube") != -1) {
+    let time = movie.time.split(":");
+    let timeString = "";
+    if(time.length == 2) {
+      timeString = `${time[0]}m${time[1]}s`;
+    }
+
+    return {
+      title: movie.title,
+      url: movie.url.search('\\?') == -1 ? `${movie.url}?t=${timeString}` : `${movie.url}&t=${timeString}`,
+      time: movie.time
+    }
+  } else {
+    return movie;
+  }
+}
+
+
+let setID = () => '_' + Math.random().toString(36).substr(2, 9);
+
+let load = (moviesList) => {
+  browser.storage.local.get().then((data) => {
+    // alert(JSON.stringify(data.movies))
+    if(data.movies != undefined) {
+      render(moviesList, reverseArray(data.movies));
+    }
+  });
+}
+
+let save = (moviesList, movie) => {
+  let checkedMovie = checkMovie(movie);
+  browser.storage.local.get().then((data) => {
+    if(Object.keys(data).length === 0) {
+      checkedMovie.id = setID();
+      browser.storage.local.set({"movies": [].concat(checkedMovie)}).then(() => {
+        load(moviesList);
+      })
+    } else {
+      let ids = data.movies.map((m) => m.id);
+      let set = false;
+      let id = "";
+      while(!set) {
+        id = setID();
+        if(ids.indexOf(id) == -1) {
+          set = true;
+        }
+      }
+      checkedMovie.id = id;
+
+      browser.storage.local.set({"movies": data.movies.concat(checkedMovie)}).then(() => {
+        load(moviesList);
+      })
+    }
+  });
+}
+
+let deleteMovie = (moviesList, id) => {
+  return () => {
+    browser.storage.local.get().then((data) => {
+      if(data.movies != undefined) {
+        let filtered = data.movies.filter((movie) => movie.id != id);
+        browser.storage.local.set({"movies": filtered}).then(() => {
+          load(moviesList);
+        })
+      }
+    });
+  }
+}
+
+let render = (list, movies) => {
+  clearList(list);
+
+  movies.forEach((movie) => {
+    // create elements
+
+    let listItem     = document.createElement("LI");
+    let titleElement = document.createElement("H2");
+    let urlElement   = document.createElement("A");
+    let timeElement  = document.createElement("TIME");
+    let deleteButton = document.createElement("BUTTON");
+
+    // set styles
+    listItem.className     = "movies-list__movie";
+    titleElement.className = "movies-list__movie-title";
+    urlElement.className   = "movies-list__movie-link";
+    timeElement.className  = "movies-list__movie-time";
+    deleteButton.className = "movies-list__movie-delete";
+    // set content
+    titleElement.textContent = movie.title;
+    urlElement.textContent   = movie.url;
+    timeElement.textContent  = movie.time;
+    // setup link
+    urlElement.href = movie.url;
+    urlElement.addEventListener("click", (event) => {
+      event.preventDefault();
+      browser.tabs.create({url: movie.url});
+    });
+    //setup delete button
+    deleteButton.addEventListener("click", deleteMovie(list, movie.id));
+    deleteButton.textContent = "delete";
+    // append elements
+    listItem.appendChild(titleElement);
+    listItem.appendChild(urlElement);
+    listItem.appendChild(timeElement);
+    listItem.appendChild(deleteButton);
+    list.appendChild(listItem);
+
+  });
+}
+
+let init = (moviesList) => {
+  load(moviesList)
+  let form = document.querySelector("#movies-form");
+
+  browser.tabs.executeScript(null, {
+    file: "/content_scripts/form.js"
+  });
+
+  let gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+  gettingActiveTab.then((tabs) => {
+    browser.tabs.sendMessage(tabs[0].id, {}).then((data) => {
+      fillForm(form, data.movie);
+    });
+  });
+}
+
+// start
+let moviesList = document.querySelector("#movies-list");
+let moviesForm = document.querySelector("#movies-form");
+
+moviesForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let form = event.target;
+  let title = form.elements.namedItem("title").value;
+  let url = form.elements.namedItem("url").value;
+  let time = form.elements.namedItem("time").value;
+  let movie = {title, url, time};
+
+  save(moviesList, movie);
+
+  form.reset();
+});
+
+// browser.storage.local.remove("movies");
+init(moviesList);
