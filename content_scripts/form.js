c6d298f323ac81c2eeea357cf39085d3bb4211c3
Tadeusz <tadeusz.m.lewandowski@gmail.com>
2017-07-02 13:42:14 +0200

    Rename project, remove description, change project page, put content code in IFI

--- content_scripts/form.js
+++ content_scripts/form.js
@@ -1,35 +1,36 @@
+(function() {
+  let getURL = () => window.location.href;
 
-let getURL = () => window.location.href;
-
-let isYoutube = (url) => {
-  return url.search("youtube.com/watch") != -1 ? true : false;
-}
-
-let youtubeData = () => {
-  let time = document.querySelector(".ytp-time-current").textContent;
-  let title = document.querySelector(".watch-title").textContent;
-  return {time, title};
-}
-
-let info = (request, sender, sendResponse) => {
-
-  let url = getURL();
-
-  let movieData = {};
-
-  if(isYoutube(url)) {
-    movieData = youtubeData();
-    movieData.url = url;
-  } else {
-    movieData = {
-      title: "",
-      url,
-      time: ""
-    }
+  let isYoutube = (url) => {
+    return url.search("youtube.com/watch") != -1 ? true : false;
   }
 
-  sendResponse({movie: movieData});
+  let youtubeData = () => {
+    let time = document.querySelector(".ytp-time-current").textContent;
+    let title = document.querySelector(".watch-title").textContent;
+    return {time, title};
+  }
 
-}
+  let info = (request, sender, sendResponse) => {
+    console.log("test")
+    let url = getURL();
 
-browser.runtime.onMessage.addListener(info);
+    let movieData = {};
+
+    if(isYoutube(url)) {
+      movieData = youtubeData();
+      movieData.url = url;
+    } else {
+      movieData = {
+        title: "",
+        url,
+        time: ""
+      }
+    }
+
+    sendResponse({movie: movieData});
+    browser.runtime.onMessage.removeListener(info);
+  }
+
+  browser.runtime.onMessage.addListener(info);
+})();
